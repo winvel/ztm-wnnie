427c12745e37806def5a5832117f93c7eda5af63
winvel <dotimaswinvel@gmail.com>
2021-03-16 19:37:43 +0800

    finalize css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       robots: robots,
-      searchField: '',
-    };
+      searchField: ''
+    }
   }
 
   onSearchChange = (event) => {
@@ -23,10 +24,10 @@ class App extends Component {
         return robots.name
           .toLowerCase()
           .includes(this.state.searchField.toLowerCase());
-      });
+      })
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const SearchBox = ({searchField}, {searchChange}) => {
+const SearchBox = ({searchField, searchChange}) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
-        type="search"
+        type='search'
         placeholder="search robots"
         onChange = {searchChange}
       />
