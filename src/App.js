c0b0724cfe1b706f5ba82287760140a8050537b3
winvel <dotimaswinvel@gmail.com>
2021-03-16 16:03:32 +0800

    updated upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    };
+  }
+
+  onSearchChange = (event) => {
+    //console.log(event.target.value);
+    this.setState({ searchField: event.target.value });
+    
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+        return robots.name
+          .toLowerCase()
+          .includes(this.state.searchField.toLowerCase());
+      });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
