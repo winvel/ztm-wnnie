8c6ca674adec0ba4c1c37c22c362c84a6dcd9665
winvel <dotimaswinvel@gmail.com>
2021-03-16 20:28:55 +0800

    organize folders

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+//import { robots } from "./robots";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    //console.log(event.target.value);
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const {robots, searchField} = this.state
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(searchField.toLowerCase());
+    });
+    
+    if (!robots.length) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 //import { robots } from './robots';
-import App from "./App";
+import App from "./containers/App";
 
 ReactDOM.render(
   <React.StrictMode>
